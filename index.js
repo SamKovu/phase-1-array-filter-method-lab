4b122cb6dba4640e88b320746f6864da34f50601
Samuel Gichuhi <gichuhi1sam@gmail.com>
2023-03-21 19:43:52 +0300

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function findMatching(array,string){
+return array.filter((element)=>{
+    let varString=string;
+    let varElement=element;
+    return (varElement.toLowerCase()===varString.toLowerCase());})
+}
+
+function fuzzyMatch(array,string){
+ return array.filter((element)=>{
+    let compare=element.substring(0,string.length);
+    return (compare==string);
+ })
+}
+
+
+function matchName(array,string){
+    return array.filter((element)=>{
+        return element.name===string})
+}
+
